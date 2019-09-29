80e5ff4c925d0298cba4a942d3ca256169fd5af3
gaston223 <g.coulibalyy@gmail.com>
2019-09-29 16:48:34 +0200

    Gestion des reservations (base)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use App\Entity\Booking;
 use App\Entity\Role;
 use App\Entity\User;
 use Faker\Factory;
@@ -95,6 +96,29 @@ class AppFixtures extends Fixture
                             ->setAd($ad);
                     $manager->persist($image);
                 }
+            // Gestion des réservations
+            for($j =1; $j <=mt_rand(0,10); $j++){
+                $booking = new Booking();
+                $createdAt = $faker->dateTimeBetween('-6months');
+                $startDate = $faker->dateTimeBetween('-3months');
+                $duration = mt_rand(3, 10);
+                $endDate = (clone $startDate)->modify("+$duration days");
+
+                $amount = $ad->getPrice() * $duration;
+                $booker= $users[mt_rand(0, count($users)-1)];
+                $comment = $faker->paragraph();
+
+                $booking->setBooker($booker)
+                    ->setAd($ad)
+                    ->setStartDate($startDate)
+                    ->setEndDate($endDate)
+                    ->setCreatedAt($createdAt)
+                    ->setAmount($amount)
+                    ->setComment($comment);
+
+                $manager->persist($booking);
+            }
+
             $manager->persist($ad);
         }
         $manager->flush();
--- src/Entity/Ad.php
+++ src/Entity/Ad.php
@@ -12,7 +12,7 @@ use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\AdRepository")
- * @ORM\HasLifecycleCallbacks
+ * @ORM\HasLifecycleCallbacks()
  * @UniqueEntity(
  *     fields={"title"},
  *     message="Une autre annonce possède déja ce titre, veuillez le modifier"
@@ -89,9 +89,15 @@ class Ad
      */
     private $author;
 
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Booking", mappedBy="ad")
+     */
+    private $bookings;
+
     public function __construct()
     {
         $this->images = new ArrayCollection();
+        $this->bookings = new ArrayCollection();
     }
 
     /**
@@ -112,6 +118,25 @@ class Ad
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+
+    public function getNotAvailableDays() {
+        $notAvailableDays = [];
+        foreach($this->bookings as $booking) {
+            // Calculer les jours qui se trouvent entre la date d'arrivée et de départ
+            $resultat = range(
+                $booking->getStartDate()->getTimestamp(),
+                $booking->getEndDate()->getTimestamp(),
+                24 * 60 * 60
+            );
+
+            $days = array_map(function($dayTimestamp){
+                return new \DateTime(date('Y-m-d', $dayTimestamp));
+            }, $resultat);
+            $notAvailableDays = array_merge($notAvailableDays, $days);
+        }
+        return $notAvailableDays;
     }
 
     public function getTitle(): ?string
@@ -237,6 +262,37 @@ class Ad
     public function setAuthor(?User $author): self
     {
         $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Booking[]
+     */
+    public function getBookings(): Collection
+    {
+        return $this->bookings;
+    }
+
+    public function addBooking(Booking $booking): self
+    {
+        if (!$this->bookings->contains($booking)) {
+            $this->bookings[] = $booking;
+            $booking->setAd($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBooking(Booking $booking): self
+    {
+        if ($this->bookings->contains($booking)) {
+            $this->bookings->removeElement($booking);
+            // set the owning side to null (unless already changed)
+            if ($booking->getAd() === $this) {
+                $booking->setAd(null);
+            }
+        }
 
         return $this;
     }
